eea1c03f5c562c720b49930749ea4d03fb4c16de
Nathanael Schaeffer <nathanael.schaeffer@gmail.com>
2015-08-23 22:45:12 +0200

    Change: since DCT transforms are not multi-threaded, use only 1 thread for planning dct using fftw (faster initialization).

--- sht_init.c
+++ sht_init.c
@@ -655,9 +655,7 @@ static int planDCT(shtns_cfg shtns)
 	double Sh0[NLAT] SSE;				// temp storage on the stack, aligned.
 
 	#ifdef OMP_FFTW
-		if (shtns->fftw_plan_mode & (FFTW_EXHAUSTIVE | FFTW_PATIENT)) {
-			fftw_plan_with_nthreads(omp_threads);
-		} else fftw_plan_with_nthreads(1);
+		fftw_plan_with_nthreads(1);		// use only 1 thread here.
 	#endif
 	// Allocate dummy Spatial Fields.
 	Sh = (double *) VMALLOC((NPHI/2 +1) * NLAT*2 * sizeof(double));
