df08d23e5c8a37381d44105ac997af0b9c718475
Nathanaël Schaeffer <nathanael.schaeffer@gmail.com>
2020-07-09 23:58:40 +0200

    Add test for Legendre-only transform in time_SHT

--- time_SHT.c
+++ time_SHT.c
@@ -888,6 +888,17 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	if (MMAX > 0) {
+		memset(Slm+LMAX+1, 0, sizeof(cplx)*(LMAX-MRES+1));
+		SH_to_spat_ml(shtns, 1, Slm0+LMAX+1, (cplx*) Sh, LMAX);
+		spat_to_SH_ml(shtns, 1, (cplx*) Sh, Slm+LMAX+1, LMAX);
+		double err = 0.0;
+		for (int i=0; i<=LMAX-MRES; i++) {
+			double t = cabs(Slm[i+LMAX+1]-Slm0[i+LMAX+1]);
+			err += t*t;
+		}
+		printf("Test Legendre only (m=%d) :: err = %g\n",MRES,sqrt(err));
+	}
 
 	shtns_create(LMAX, MMAX, MRES, shtnorm);		// test memory allocation and management.
 //	shtns_create_with_grid(shtns, MMAX/2, 1);
