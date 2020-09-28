b62ea0bc701eaf6cc1c579c0c8e25f363d064616
Nathanaël Schaeffer <nathanael.schaeffer@gmail.com>
2020-09-28 09:25:21 +0200

    Add reference image for shallow_water.py into the doxygen doc.

--- examples/shallow_water.py
+++ examples/shallow_water.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 
-##
-## "Non-linear barotropically unstable shallow water test case"
-## Example provided by Jeffrey Whitaker
-## https://gist.github.com/jswhit/3845307
-##
-## Running the script should pop up a window with this image:
-##  https://i.imgur.com/CEzHJ0g.png
-##
+#
+# "Non-linear barotropically unstable shallow water test case"
+# Example provided by Jeffrey Whitaker
+# https://gist.github.com/jswhit/3845307
+#
+# Running the script should pop up a window with this image:
+#  https://i.imgur.com/CEzHJ0g.png
+#
 
 import numpy as np
 import shtns
